3c6c7ef6d7c51080eaa9466fca51e2ef01a6650a
maminkss <57598118+maminkss@users.noreply.github.com>
2023-01-11 11:07:24 +0300

    Delete soru1.c

--- soru1.c
+++
@@ -1,8 +0,0 @@
-// "C Programlama dünyasýna hos geldiniz" mesajýný ekrana yazdýran program.
-
-#include <stdio.h> // KÜTÜPHANE TANIMLAMA //
-
-int main () {
-		printf("C Programlama dunyasina hos geldiniz."); // EKRANA YAZDIRMA //
-		return 0;
-}
